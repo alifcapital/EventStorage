6e6b772eab70fdc7ccf021b04e64c464fde5a4a2
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-03-19 13:55:58 +0500

    Implemented PublishAsync

--- src/Outbox/Managers/IOutboxEventManager.cs
+++ src/Outbox/Managers/IOutboxEventManager.cs
@@ -36,7 +36,8 @@ public interface IOutboxEventManager : IDisposable
     /// </summary>
     /// <param name="outboxEvent">Event to send</param>
     /// <param name="eventProvider">Provider type of sending event</param>
-    /// <param name="namingPolicyType">Name of the naming policy type for serializing and deserializing properties of Event. Default value is "PascalCase". It can be one of "PascalCase", "CamelCase", "SnakeCaseLower", "SnakeCaseUpper", "KebabCaseLower", or "KebabCaseUpper".</param>
+    /// <param name="namingPolicyType">Name of the naming policy type for serializing and deserializing properties of Event. Default value is "PascalCase".
+    /// It can be one of "PascalCase", "CamelCase", "SnakeCaseLower", "SnakeCaseUpper", "KebabCaseLower", or "KebabCaseUpper".</param>
     /// <typeparam name="TOutboxEvent">Event type that must implement from the TOutboxEvent</typeparam>
     /// <returns>Returns true if it was entered successfully or false if the value is duplicated. It can throw an exception if something goes wrong.</returns>
     public Task<bool> StoreAsync<TOutboxEvent>(TOutboxEvent outboxEvent, EventProviderType eventProvider, 
@@ -45,12 +46,15 @@ public interface IOutboxEventManager : IDisposable
 
     /// <summary>
     /// Stores the event to the database immediately.
-    /// The event provider will be identified based on existing event publishers and execute a publisher of all of them. But if there is no publisher, it will just add an error log and return false.
+    /// The event provider will be identified based on existing event publishers and execute a publisher of all of them.
+    /// But if there is no publisher, it will just add an error log and return false.
     /// </summary>
     /// <param name="outboxEvent">Event to store</param>
-    /// <param name="namingPolicyType">Name of the naming policy type for serializing and deserializing properties of Event. Default value is "PascalCase". It can be one of "PascalCase", "CamelCase", "SnakeCaseLower", "SnakeCaseUpper", "KebabCaseLower", or "KebabCaseUpper".</param>
+    /// <param name="namingPolicyType">Name of the naming policy type for serializing and deserializing properties of Event. Default value is "PascalCase".
+    /// It can be one of "PascalCase", "CamelCase", "SnakeCaseLower", "SnakeCaseUpper", "KebabCaseLower", or "KebabCaseUpper".</param>
     /// <typeparam name="TOutboxEvent">Event type that must implement from the TOutboxEvent</typeparam>
-    /// <returns>Returns true if it was entered successfully or false if the value is duplicated or if event does not have publisher. It can throw an exception if something goes wrong.</returns>
+    /// <returns>Returns true if it was entered successfully or false if the value is duplicated or if event does not have publisher.
+    /// It can throw an exception if something goes wrong.</returns>
     public Task<bool> StoreAsync<TOutboxEvent>(TOutboxEvent outboxEvent, NamingPolicyType namingPolicyType = NamingPolicyType.PascalCase)
         where TOutboxEvent : IOutboxEvent;
 
