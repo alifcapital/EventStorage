8b44680059beca2636d01917501216fc821b0bb4
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-11-28 11:43:09 +0500

    Remove Parallelizable attribute from CleanUpProcessedInbox/OutboxEventsJobTests for improved test parallelism

--- tests/UnitTests/Inbox/CleanUpProcessedInboxEventsJobTests.cs
+++ tests/UnitTests/Inbox/CleanUpProcessedInboxEventsJobTests.cs
@@ -3,6 +3,5 @@ using EventStorage.Inbox.Repositories;
 
 namespace EventStorage.Tests.UnitTests.Inbox;
 
-[Parallelizable]
 internal class CleanUpProcessedInboxEventsJobTests : 
     BaseCleanUpProcessedEventsJobTests<IInboxRepository, InboxMessage>;
